ed9d1ba6e2ee43912981195697d739c5e94333be
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-11-24 10:45:27 +0530

    Intro to NodeJS

---
+++ ch-1/index.js
@@ -0,0 +1,15 @@
+console.log("hello word!!");
+console.log(__filename);
+console.log(__dirname);
+
+setTimeout(()=>{
+    console.log("hello rahul");
+},2000)
+
+setInterval(()=>{
+    console.log("hey coder!!");
+    
+},5000)
+
+
+
