f6b954b84256c6991015d1c7e5ebd4632b72d6a5
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-01 11:50:23 +0530

    nodejs

--- server/server.js
+++ server/server.js
@@ -1,19 +1,58 @@
-const { log } = require("console");
+const fs = require("fs");
 const http = require("http");
 
-const server =http.createServer((req , res) => {
-    res.write(" <h1>WELCOME TO MY SERVER !!! </h1>");
-    res.end();
+const server = http.createServer((req, res) => {
 
-});
 
-server.listen(12000 ,(err) => {
-    if(err)
-    {
-        console.log("server is not started .!!!!!!" , err);
-        return false;
+    const msg = `New user: ${new Date()} | IP Address: ${req.socket.remoteAddress}\n`;
+
+
+    fs.appendFile("./log.txt", msg, (err) => {
+        if (err) console.log(err);
+    });
+
+    console.log(req.url);
+
+    let fileName = "";
+
+    switch (req.url) {
+        case `/`:
+            fileName = "index.html";
+            break;
+        case `/about`:
+            fileName = "about.html";
+            break;
+        case `/contact`:
+            fileName = "contact.html";
+            break;
+             case `/services`:
+            fileName = "services.html";
+            break;
+        default:
+            fileName = "notfound.html";
+
     }
 
-    console.log("server is started .!!!!!!");
-    
-})
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            console.log("Error :", err);
+
+        }
+        else 
+        {
+           res.end (data);
+            
+        }
+    });
+
+    // res.end("Server working!");
+});
+
+server.listen(12000, (err) => {
+    if (err) {
+        console.log("Server is not started!", err);
+        return;
+    }
+
+    console.log("Server is started on port 12000");
+});
